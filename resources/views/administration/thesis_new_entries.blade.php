a0de974c79f033c77d9f9d9ce8455809cbd7e582
Thomas-git-hub <thomasallenebonaobra.escoto@bicol-u.edu.ph>
2024-11-07 14:58:06 +0800

    110724 246

--- resources/views/administration/thesis_new_entries.blade.php
+++ resources/views/administration/thesis_new_entries.blade.php
@@ -13,29 +13,40 @@
                     <div class="row">
                         <label for="defaultFormControlInput" class="form-label">Student Name</label>
                     </div>
-                    <div class="row">
-                        <div class="col-md">
-                            {{-- <label for="defaultFormControlInput" class="form-label">Name</label> --}}
-                            <input type="text" class="form-control" id="defaultFormControlInput" placeholder="First Name" aria-describedby="defaultFormControlHelp" />
-                        </div>
-                        <div class="col-md">
-                            {{-- <label for="defaultFormControlInput" class="form-label">Name</label> --}}
-                            <input type="text" class="form-control" id="defaultFormControlInput" placeholder="Middle Name" aria-describedby="defaultFormControlHelp" />
-                        </div>
-                        <div class="col-md">
-                            {{-- <label for="defaultFormControlInput" class="form-label">Name</label> --}}
-                            <input type="text" class="form-control" id="defaultFormControlInput" placeholder="Suffix. (optional)" aria-describedby="defaultFormControlHelp" />
-                        </div>
-                        <div class="col-md">
-                            {{-- <label for="defaultFormControlInput" class="form-label">Name</label> --}}
-                            <input type="text" class="form-control" id="defaultFormControlInput" placeholder="Last Name" aria-describedby="defaultFormControlHelp" />
+                    <div id="searchStudentDiv">
+                        <div class="row" id="searchStudentRow">
+                            <div class="col-md-10">
+                                <select class="form-select" id="student" name="student">
+                                    <option value="">Search/Select Student</option>
+                                    <!-- Options for advisers will be populated here -->
+                                </select>
+                            </div>
+                            <div class="col-md-2">
+                                <button class="btn btn-label-primary w-100" id="addStudentButton"><i class='bx bx-user-plus'></i>Add Student</button>
+                            </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-md d-flex justify-content-end">
-                            <button type="button" class="btn btn-icon me-2 btn-primary">
-                                <span class="tf-icons bx bx-search-alt bx-22px"></span>
-                            </button>
+                    <div id="inputGroupStudentDiv" style="display: none">
+                        <div class="row" id="inputGroupStudentRow">
+                            <div class="col-md">
+                                <button class="btn btn-label-warning d-flex justify content-end w-100" id="cancelStudentButton"><i class='bx bx-search-alt'></i>Search</button>
+                            </div>
+                            <div class="col-md">
+                                {{-- <label for="defaultFormControlInput" class="form-label">Name</label> --}}
+                                <input type="text" class="form-control" id="defaultFormControlInput" placeholder="First Name" aria-describedby="defaultFormControlHelp" />
+                            </div>
+                            <div class="col-md">
+                                {{-- <label for="defaultFormControlInput" class="form-label">Name</label> --}}
+                                <input type="text" class="form-control" id="defaultFormControlInput" placeholder="Middle Name" aria-describedby="defaultFormControlHelp" />
+                            </div>
+                            <div class="col-md">
+                                {{-- <label for="defaultFormControlInput" class="form-label">Name</label> --}}
+                                <input type="text" class="form-control" id="defaultFormControlInput" placeholder="Suffix. (optional)" aria-describedby="defaultFormControlHelp" />
+                            </div>
+                            <div class="col-md">
+                                {{-- <label for="defaultFormControlInput" class="form-label">Name</label> --}}
+                                <input type="text" class="form-control" id="defaultFormControlInput" placeholder="Last Name" aria-describedby="defaultFormControlHelp" />
+                            </div>
                         </div>
                     </div>
                     <div class="row mt-3">
@@ -83,6 +94,52 @@
                             </select>
                         </div>
                     </div>
+
+                    <!-- Member -->
+                    <div class="row mt-3">
+                        <label for="defaultFormControlInput" class="form-label">Member</label>
+                    </div>
+
+                    <div class="mb-4 memberFormField" id="memberFormField_1">
+                        <div class="row">
+                            <div class="col-md">
+                                <select class="form-select degree" name="degree">
+                                    <option value="">Select Member Type</option>
+                                    <option value="masteral">Internal Member</option>
+                                    <option value="doctoral">External Member</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="mt-2 searchMemberDiv">
+                            <div class="row searchStudentRow">
+                                <div class="col-md-10">
+                                    <select class="form-select member" name="member">
+                                        <option value="">Search/Select Member</option>
+                                        <!-- Options for advisers will be populated here -->
+                                    </select>
+                                </div>
+                                <div class="col-md-2">
+                                    <button class="btn btn-label-primary w-100 addMemberButton"><i class='bx bx-user-plus'></i>Create Member</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mt-2 inputGroupMemberDiv" style="display: none">
+                            <div class="row inputGroupMemberRow">
+                                <div class="col-md">
+                                    <button class="btn btn-label-warning w-100 cancelMemberButton"><i class='bx bx-search-alt'></i>Search a Member</button>
+                                </div>
+                                <!-- Name fields -->
+                                <div class="col-md"><input type="text" class="form-control" placeholder="First Name" /></div>
+                                <div class="col-md"><input type="text" class="form-control" placeholder="Middle Name" /></div>
+                                <div class="col-md"><input type="text" class="form-control" placeholder="Suffix (optional)" /></div>
+                                <div class="col-md"><input type="text" class="form-control" placeholder="Last Name" /></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- New Button for Adding Forms -->
+                    <button class="btn btn-primary mt-3" id="addNewMemberFormButton">Add New Member Form</button>
+
                 </div>
             </div>
         </div>
@@ -110,6 +167,67 @@
 
 @section('components.specific_page_scripts')
 <script>
+    // Student
+    $(document).ready(function() {
+        $('#addStudentButton').click(function() {
+            $('#searchStudentDiv').hide();
+            $('#inputGroupStudentDiv').show();
+            $('#searchStudentDiv input').val('');
+            $('#inputGroupStudentDiv input').val('');
+        });
+
+        $('#cancelStudentButton').click(function() {
+            $('#inputGroupStudentDiv').hide();
+            $('#searchStudentDiv').show();
+            $('#searchStudentDiv input').val('');
+            $('#inputGroupStudentDiv input').val('');
+        });
+    });
+
+
+    // adding of member
+    $(document).ready(function() {
+        let formCount = 1;
+        const maxForms = 4;
+
+        // Add new member form when the "Add New Member Form" button is clicked
+        $('#addNewMemberFormButton').click(function(e) {
+            e.preventDefault();
+
+            if (formCount < maxForms) {
+                formCount++;
+                const newForm = $('#memberFormField_1').clone().removeAttr('id');
+
+                // Clear inputs in the cloned form and reset visibility states
+                newForm.find('input').val('');
+                newForm.find('.searchMemberDiv').show();
+                newForm.find('.inputGroupMemberDiv').hide();
+
+                // Insert cloned form
+                $('#memberFormField_1').after(newForm);
+            } else {
+                alert("Maximum of 4 member forms reached.");
+            }
+        });
+
+        // Delegated event for show/hide toggle (covers original and duplicated forms)
+        $(document).on('click', '.addMemberButton', function(e) {
+            e.preventDefault();
+            $(this).closest('.searchMemberDiv').hide();
+            $(this).closest('.memberFormField').find('.inputGroupMemberDiv').show();
+        });
+
+        $(document).on('click', '.cancelMemberButton', function(e) {
+            e.preventDefault();
+            $(this).closest('.inputGroupMemberDiv').hide();
+            $(this).closest('.memberFormField').find('.searchMemberDiv').show();
+        });
+    });
+
+
+
+
+    // Datatables
     $(document).ready(function() {
         // Sample data - replace with actual data source
         const studentsData = [
@@ -207,6 +325,11 @@
     // Select2 searchable fields for Adviser and Chairperson
     $(document).ready(function() {
         // Initialize Select2 for searchable dropdowns
+        $('#student').select2({
+            placeholder: "Search/Select Student",
+            allowClear: true
+        });
+
         $('#adviser').select2({
             placeholder: "Search/Select Adviser",
             allowClear: true
@@ -216,6 +339,13 @@
             placeholder: "Search/Select Chairperson",
             allowClear: true
         });
+
+        $('#member').select2({
+            placeholder: "Search/Select Member",
+            allowClear: true
+        });
     });
+
+
 </script>
 @endsection
